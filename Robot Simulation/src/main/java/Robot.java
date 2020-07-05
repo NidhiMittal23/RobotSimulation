7ae098ba402ccbbfd58eeb6794e78e4918c2191c
Nidhi Mittal <nidhi.mittal@greyamp.com>
2020-07-05 14:29:46 +0530

    first commit

---
+++ Robot Simulation/src/main/java/Robot.java
@@ -0,0 +1,35 @@
+public class Robot {
+    private int x;
+    private int y;
+    private char direction;
+
+    public Robot(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+}
