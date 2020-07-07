9aadc58647e339ddb89fee63cb7297908f5a0c1e
Nidhi Mittal <nidhi.mittal@greyamp.com>
2020-07-07 11:33:19 +0530

    Rolling back the test class changes

--- Robot Simulation/src/SimulationTest.java
+++ Robot Simulation/src/SimulationTest.java
@@ -1,5 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-class SimulationTest {
+public class SimulationTest {
+    Robot robot;
+    Simulation simulation;
+    @Before
+    public void setup(){
+        simulation = new Simulation();
+    }
+
+    @Test
+    public void testSimulateWhenInput73WTravereRAALALOutput94W(){
+        robot= new Robot(7,3,'N');
+        String traverse = "RAALAL";
+        String result = simulation.simulate(robot,traverse.toCharArray());
+        Assert.assertEquals("9 4 W",result);
+    }
+
 
 }
